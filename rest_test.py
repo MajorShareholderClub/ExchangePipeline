4b13ed371d0b9a0e0136e798e3653c4cb0d62ee0
sky81219 <=>
2024-10-25 17:32:21 +0900

    sytle:   ✔

--- rest_test.py
+++ rest_test.py
@@ -24,6 +24,13 @@ async def k_btc_present_start() -> None:
     await KoreaExchangeRestAPI().total_pull_request("BTC")
 
 
+async def n_btc_present_start() -> None:
+    """
+    ethereum kafak stream
+    """
+    await NEExchangeRestAPI().total_pull_request("BTC")
+
+
 async def be_present_gether() -> None:
     """
     kafka async stream
@@ -31,6 +38,7 @@ async def be_present_gether() -> None:
     tasks = [
         asyncio.create_task(f_btc_present_start()),
         asyncio.create_task(k_btc_present_start()),
+        asyncio.create_task(n_btc_present_start()),
     ]
     await asyncio.gather(*tasks, return_exceptions=False)
 
